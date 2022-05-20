82926dd0153665e41e7556fc29c8f4796e8b891a
CodeAndCulinary <alvar@hotmail.fi>
2022-05-20 16:55:28 +0300

    initial commit

--- pages/popper.js
+++ pages/popper.js
@@ -6,12 +6,7 @@ function MyPopover() {
       <Popover.Button>Solutions</Popover.Button>
 
       <Popover.Panel className="absolute z-10">
-        <div className="grid grid-cols-2">
-          <a href="/analytics">Analytics</a>
-          <a href="/engagement">Engagement</a>
-          <a href="/security">Security</a>
-          <a href="/integrations">Integrations</a>
-          
+        <div className="grid grid-cols-2">          
           <a href="/analytics">Analytics</a>
           <a href="/engagement">Engagement</a>
           <a href="/security">Security</a>
