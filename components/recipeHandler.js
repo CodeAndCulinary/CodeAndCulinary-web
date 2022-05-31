3abfb57a629cc5f9b6422f15d2c5363d2dac3899
Stormle <alvar.stara@gmail.com>
2022-05-31 17:00:08 +0300

    SEO duplicate key handling

--- components/recipeHandler.js
+++ components/recipeHandler.js
@@ -2,9 +2,51 @@ import React from 'react'
 import NavBar from './Nav';
 import Head from 'next/head'
 function recipeHandler(recipeData) {
+
     return (
-        <p>{recipeData.toString()}</p>
+        <>
+            <Tags tagsContent={ recipeData.tags }/>
+            <ul>
+                {recipeData.content.map((recipeObj) =>(
+                    <RecipeObjectHandler recipeObject={recipeObj}/>
+                ))}
+            </ul>
+        </>
     );
 };
+
+const RecipeObjectHandler = ({ recipeObject }) => {
+    if (recipeObject.type === "text") {return await textHandler(recipeObject.content) }
+    if (recipeObject.type === "header") {return await headerHandler(recipeObject.content) }
+    if (recipeObject.type === "subheader") {return await subHeaderHandler(recipeObject.content) }
+    if (recipeObject.type === "list") {return await listHandler(recipeObject.content) }
+    if (recipeObject.type === "image") {return await imageHandler(recipeObject.content) }
+    if (recipeObject.type === "webm") {return await webmHandler(recipeObject.content) }
+    return (<></>)
+}
+
+async function headerHandler(content) {
+
+}
+async function textHandler(content) {
+
+}
+async function subHeaderHandler(content) {
+
+}
+async function listHandler(content) {
+
+}
+async function webmHandler(content) {
+
+}
+async function imageHandler(content) {
+
+}
+const Tags = ({ tagsContent }) => {
+    return (
+        <Head><title>{tagsContent}</title></Head>
+    )
+}
 
 module.exports = recipeHandler;
